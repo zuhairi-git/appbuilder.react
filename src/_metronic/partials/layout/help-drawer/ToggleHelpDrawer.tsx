e6419bc73570067bdaa8f16f2660ba2bdab4be84
Ali Al-Zuhairi <zohairi@live.com>
2022-11-24 20:19:42 +0200

    remove demo, help, purchase

--- src/_metronic/partials/layout/help-drawer/ToggleHelpDrawer.tsx
+++ src/_metronic/partials/layout/help-drawer/ToggleHelpDrawer.tsx
@@ -3,7 +3,7 @@ import React, {FC} from 'react'
 const ToggleHelpDrawer: FC = () => (
   <button
     id='kt_help_toggle'
-    className='engage-help-toggle btn btn-flex h-35px bg-body btn-color-gray-700 btn-active-color-gray-900 shadow-sm px-5 rounded-top-0'
+    className='d-none engage-help-toggle btn btn-flex h-35px bg-body btn-color-gray-700 btn-active-color-gray-900 shadow-sm px-5 rounded-top-0'
     title='Learn & Get Inspired'
     data-bs-toggle='tooltip'
     data-bs-placement='left'
