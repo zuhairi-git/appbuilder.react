541b07f102973b974e289a362987eee322c85fb4
Ali Al-Zuhairi <zohairi@live.com>
2022-11-27 12:38:42 +0200

    add progressbar

--- src/app/app-builder/AppProjects.tsx
+++ src/app/app-builder/AppProjects.tsx
@@ -1,4 +1,6 @@
 import React from 'react'
+import { Badge, ProgressBar } from 'react-bootstrap'
+import { Link } from 'react-router-dom'
 
 export function AppProjects() {
 	return (
@@ -6,7 +8,7 @@ export function AppProjects() {
 			{/*begin::Col*/}
 			<div className="col-md-6 col-xl-4">
 				{/*begin::Card*/}
-				<a href={"./app/app-builder/AppDefine"} className="card border-hover-primary">
+				<Link to={"../app/app-builder/AppDefine"} className="card border-hover-primary">
 					{/*begin::Card header*/}
 					<div className="card-header border-0 pt-9">
 						{/*begin::Card Title*/}
@@ -31,7 +33,7 @@ export function AppProjects() {
 						<div className="fs-3 fw-bold text-dark">Smart Banking</div>
 						{/*end::Name*/}
 						{/*begin::Description*/}
-						<p className="text-gray-400 fw-semibold fs-5 mt-1 mb-7">Made by Appliction Builder | Powered by Center UX</p>
+						<p className="text-gray-400 fw-semibold fs-5 mt-1 mb-7">Made by Appliction Builder</p>
 						{/*end::Description*/}
 						{/*begin::Info*/}
 						<div className="d-flex flex-wrap mb-5">
@@ -41,16 +43,16 @@ export function AppProjects() {
 								<div className="fw-semibold text-gray-400">Due Date</div>
 							</div>
 							{/*end::Due*/}
-							{/*begin::Budget*/}
-							<div className="border border-gray-300 border-dashed rounded min-w-125px py-3 px-4 mb-3">
-								<div className="fs-6 text-gray-800 fw-bold">$284,900.00</div>
-								<div className="fw-semibold text-gray-400">Budget</div>
-							</div>
-							{/*end::Budget*/}
 						</div>
 						{/*end::Info*/}
 
 						{/*begin::Progress*/}
+						<div className="row">
+							<div className='col-md-12'>
+								<ProgressBar striped animated variant="primary" now={80} label={`80`} visuallyHidden className='mb-5' />
+							</div>
+							<div className='d-none col-md-2' style={{ marginTop: '-5px'}}><Badge pill bg="dark">80</Badge></div>
+                        </div>
 
 						{/*end::Progress*/}
 						
@@ -70,7 +72,7 @@ export function AppProjects() {
 						{/*end::Users*/}
 					</div>
 					{/*end:: Card body*/}
-				</a>
+				</Link>
 				{/*end::Card*/}
 			</div>
 			{/*end::Col*/}
