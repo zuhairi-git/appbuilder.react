8bd695ea8c2fc3d6d08227d894aa900d6638f515
Ali Al-Zuhairi <zohairi@live.com>
2022-12-01 11:31:24 +0200

    Add TopCard for cleaner code

--- src/app/app-builder/AppProjectHomeHeader.tsx
+++ src/app/app-builder/AppProjectHomeHeader.tsx
@@ -4,6 +4,7 @@ import { KTSVG, toAbsoluteUrl } from '../../_metronic/helpers'
 import { Link } from 'react-router-dom'
 import { useLocation } from 'react-router-dom'
 import { AppDefine } from './AppDefine'
+import { TopCard } from './TopCard'
 
 const AppProjectHomeHeader: React.FC = () => {
   const location = useLocation()
@@ -12,67 +13,7 @@ const AppProjectHomeHeader: React.FC = () => {
     <div>
       <div className='card mb-5 mb-xl-10'>
         <div className='card-body pt-9 pb-0'>
-          <div className='d-flex flex-wrap flex-sm-nowrap mb-3'>
-            <div className='me-7 mb-4'>
-              <div className='symbol symbol-100px symbol-lg-160px symbol-fixed position-relative'>
-                <img src={toAbsoluteUrl('/media/avatars/ali.jpg')} alt='Ali' />
-                <div className='position-absolute translate-middle bottom-0 start-100 mb-6 bg-success rounded-circle border border-4 border-white h-20px w-20px'></div>
-              </div>
-            </div>
-
-            <div className='flex-grow-1'>
-              <div className='d-flex justify-content-between align-items-start flex-wrap mb-2'>
-                <div className='d-flex flex-column'>
-                  <div className='d-flex align-items-center mb-2'>
-                    <a href='#' className='text-gray-800 text-hover-primary fs-2 fw-bolder me-1'>
-                      Ali Zuhairi
-                    </a>
-                    <a href='#'>
-                      <KTSVG
-                        path='/media/icons/duotune/general/gen026.svg'
-                        className='svg-icon-1 svg-icon-primary'
-                      />
-                    </a>
-                  </div>
-
-                  <div className='d-flex flex-wrap fw-bold fs-6 mb-4 pe-2'>
-                    <a
-                      href='#'
-                      className='d-flex align-items-center text-gray-400 text-hover-primary me-5 mb-2'
-                    >
-                      <KTSVG
-                        path='/media/icons/duotune/communication/com006.svg'
-                        className='svg-icon-4 me-1'
-                      />
-                      Developer
-                    </a>
-                    <a
-                      href='#'
-                      className='d-flex align-items-center text-gray-400 text-hover-primary me-5 mb-2'
-                    >
-                      <KTSVG
-                        path='/media/icons/duotune/general/gen018.svg'
-                        className='svg-icon-4 me-1'
-                      />
-                      SF, Bay Area
-                    </a>
-                    <a
-                      href='#'
-                      className='d-flex align-items-center text-gray-400 text-hover-primary mb-2'
-                    >
-                      <KTSVG
-                        path='/media/icons/duotune/communication/com011.svg'
-                        className='svg-icon-4 me-1'
-                      />
-                      ali@appbuilder.cc
-                    </a>
-                  </div>
-                </div>
-
-              </div>
-            </div>
-          </div>
-
+          <TopCard />
           <div className='d-flex overflow-auto h-55px'>
             <ul className='nav nav-stretch nav-line-tabs nav-line-tabs-2x border-transparent fs-5 fw-bolder flex-nowrap'>
               <li className='nav-item'>
