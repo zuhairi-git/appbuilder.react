9c2e047bc25fa061cf788220fba5755a5d68b153
Ali Al-Zuhairi <zohairi@live.com>
2022-12-02 14:49:14 +0200

    Update DashboardWrapper.tsx

--- src/app/pages/dashboard/DashboardWrapper.tsx
+++ src/app/pages/dashboard/DashboardWrapper.tsx
@@ -1,8 +1,8 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
-import {FC} from 'react'
-import {useIntl} from 'react-intl'
-import {toAbsoluteUrl} from '../../../_metronic/helpers'
-import {PageTitle} from '../../../_metronic/layout/core'
+import { FC } from 'react'
+import { useIntl } from 'react-intl'
+import { toAbsoluteUrl } from '../../../_metronic/helpers'
+import { PageTitle } from '../../../_metronic/layout/core'
 import {
   ListsWidget2,
   ListsWidget3,
@@ -21,9 +21,9 @@ import {
 const DashboardPage: FC = () => (
   <>
     {/* begin::Row */}
-    <div className='row g-5 g-xl-10 mb-5 mb-xl-10'>
+    <div className='row g-5 g-xl-12 mb-5 mb-xl-10'>
       {/* begin::Col */}
-      <div className='col-md-6 col-lg-6 col-xl-6 col-xxl-3 mb-md-5 mb-xl-10'>
+      <div className='col-md-6 col-lg-6 col-xl-6 col-xxl-6 mb-md-5 mb-xl-10'>
         <CardsWidget20
           className='h-md-50 mb-5 mb-xl-10'
           description='Active Projects'
@@ -41,18 +41,18 @@ const DashboardPage: FC = () => (
       </div>
       {/* end::Col */}
 
-      {/* begin::Col */}
-      <div className='col-md-6 col-lg-6 col-xl-6 col-xxl-3 mb-md-5 mb-xl-10'>
-        <CardsWidget17 className='h-md-50 mb-5 mb-xl-10' />
-        <ListsWidget26 className='h-lg-50' />
+      <div className='d-none '>
+        {/* begin::Col */}
+        <div className='col-md-6 col-lg-6 col-xl-6 col-xxl-3 mb-md-5 mb-xl-10'>
+          <CardsWidget17 className='h-md-50 mb-5 mb-xl-10' />
+          <ListsWidget26 className='h-lg-50' />
+        </div>
+        {/* end::Col */}
       </div>
-      {/* end::Col */}
 
-      {/* begin::Col */}
       <div className='col-xxl-6'>
-        <EngageWidget10 className='h-md-100' />
+        <ListsWidget3 className='card-xxl-stretch mb-xl-3' />
       </div>
-      {/* end::Col */}
     </div>
     {/* end::Row */}
 
@@ -73,10 +73,7 @@ const DashboardPage: FC = () => (
     {/* end::Row */}
 
     {/* begin::Row */}
-    <div className='row gy-5 gx-xl-8'>
-      <div className='col-xxl-4'>
-        <ListsWidget3 className='card-xxl-stretch mb-xl-3' />
-      </div>
+    <div className='d-none row gy-5 gx-xl-8'>
       <div className='col-xl-8'>
         <TablesWidget10 className='card-xxl-stretch mb-5 mb-xl-8' />
       </div>
@@ -88,17 +85,13 @@ const DashboardPage: FC = () => (
       <div className='col-xl-4'>
         <ListsWidget2 className='card-xl-stretch mb-xl-8' />
       </div>
-      <div className='col-xl-4'>
-        <ListsWidget6 className='card-xl-stretch mb-xl-8' />
-      </div>
-      <div className='col-xl-4'>
-        <ListsWidget4 className='card-xl-stretch mb-5 mb-xl-8' items={5} />
-        {/* partials/widgets/lists/_widget-4', 'class' => 'card-xl-stretch mb-5 mb-xl-8', 'items' => '5' */}
+      <div className='col-xxl-8'>
+        <TablesWidget5 className='card-xxl-stretch mb-5 mb-xxl-8' />
       </div>
     </div>
     {/* end::Row */}
 
-    <div className='row g-5 gx-xxl-8'>
+    <div className='d-none row g-5 gx-xxl-8'>
       <div className='col-xxl-4'>
         <MixedWidget8
           className='card-xxl-stretch mb-xl-3'
@@ -106,9 +99,14 @@ const DashboardPage: FC = () => (
           chartHeight='150px'
         />
       </div>
-      <div className='col-xxl-8'>
-        <TablesWidget5 className='card-xxl-stretch mb-5 mb-xxl-8' />
+      <div className='col-xl-4'>
+        <ListsWidget6 className='card-xl-stretch mb-xl-8' />
       </div>
+      <div className='col-xl-4'>
+        <ListsWidget4 className='card-xl-stretch mb-5 mb-xl-8' items={5} />
+        {/* partials/widgets/lists/_widget-4', 'class' => 'card-xl-stretch mb-5 mb-xl-8', 'items' => '5' */}
+      </div>
+
     </div>
   </>
 )
@@ -117,10 +115,10 @@ const DashboardWrapper: FC = () => {
   const intl = useIntl()
   return (
     <>
-      <PageTitle breadcrumbs={[]}>{intl.formatMessage({id: 'MENU.DASHBOARD'})}</PageTitle>
+      <PageTitle breadcrumbs={[]}>{intl.formatMessage({ id: 'MENU.DASHBOARD' })}</PageTitle>
       <DashboardPage />
     </>
   )
 }
 
-export {DashboardWrapper}
+export { DashboardWrapper }
