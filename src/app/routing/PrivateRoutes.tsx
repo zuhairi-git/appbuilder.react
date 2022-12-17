de7635583df9671386b88c9ea20644cb615bae85
Ali Al-Zuhairi <zohairi@live.com>
2022-12-17 11:26:26 +0200

    Update PrivateRoutes.tsx

--- src/app/routing/PrivateRoutes.tsx
+++ src/app/routing/PrivateRoutes.tsx
@@ -26,7 +26,7 @@ const PrivateRoutes = () => {
     <Routes>
       <Route element={<MasterLayout />}>
         {/* Redirect to Dashboard after success login/registartion */}
-        <Route path='auth/*' element={<Navigate to='/dashboard' />} />
+        <Route path='auth/*' element={<Navigate to='/app-projects' />} />
         {/* Pages */}
         <Route path="app-define" element={<AppDefine />} />
         <Route path="app-design" element={<AppDesign />} />
